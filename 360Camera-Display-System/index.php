3ba72974e13779b081bd7cbf5acbcdf50af84174
TaigaYamabe <taiga.ybaseball@icloud.com>
2023-10-02 02:07:00 +0900

    オブジェクト配置機能の追加

--- 360Camera-Display-System/index.php
+++ 360Camera-Display-System/index.php
@@ -10,7 +10,22 @@ $path = './Models/';
 // ファイルがアップロードされているかと、POST通信でアップロードされたかを確認
 if( !empty($_FILES['file1']['tmp_name']) && is_uploaded_file($_FILES['file1']['tmp_name']) ) {
 	// ファイルを指定したパスへ保存する
-	if( move_uploaded_file( $_FILES['file1']['tmp_name'], $path.'upload.glb') ) {
+	$name = $_FILES['file1']['name'];
+	// if( move_uploaded_file( $_FILES['file1']['tmp_name'], "$path/$name") ) {
+	// 	echo 'アップロードされたファイルを保存しました。';
+	// } else {
+	// 	echo 'アップロードされたファイルの保存に失敗しました。';
+	// }
+
+	if( move_uploaded_file( $_FILES['file1']['tmp_name'], $path.'upload.obj') ) {
+		echo 'アップロードされたファイルを保存しました。';
+	} else {
+		echo 'アップロードされたファイルの保存に失敗しました。';
+	}
+}
+if( !empty($_FILES['file2']['tmp_name']) && is_uploaded_file($_FILES['file2']['tmp_name']) ) {
+	// ファイルを指定したパスへ保存する
+	if( move_uploaded_file( $_FILES['file2']['tmp_name'], $path.'space.glb') ) {
 		echo 'アップロードされたファイルを保存しました。';
 	} else {
 		echo 'アップロードされたファイルの保存に失敗しました。';
